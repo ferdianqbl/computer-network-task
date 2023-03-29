91b1f44ced16b2328d86f056be1448fb9c5addbc
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-29 16:25:02 +0700

    add multithreading secure and  not secure

---
+++ midterm/perftest.sh
@@ -0,0 +1,5 @@
+#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testingab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing.txt
+
+
+ab -n 1000 -c 50 http://localhost:8889/testing.txt
+
