6125926b2012d285da9ae739c566c6379fbc546f
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-30 22:20:23 +0700

    bug fixing server thread secure and add new command in perftest for secure testing

--- midterm/perftest.sh
+++ midterm/perftest.sh
@@ -8,6 +8,8 @@ ab -n 1000 -c 10 http://localhost:8889/testing.txt
 # ab -n 1000 -c 150 http://localhost:8889/testing.txt
 # ab -n 1000 -c 200 http://localhost:8889/testing.txt
 # abs -n 100 -c 1 https://localhost:8889/testing.txt
+# abs -n 1000 -c 10 -l -k -f SSL2 https://localhost:8889/testing.txt
+# abs -n 1000 -c 10 -l -k -f TLS1.2 https://localhost:8889/testing.txt
 # abs -n 100 -c 10 https://www.apache.org/
 # abs -l -n 5 https://www.google.com/
 
