b76a41daf40792823bee7bbe62671123457e4e67
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-30 18:24:14 +0700

    create server multiprocess

--- midterm/perftest.sh
+++ midterm/perftest.sh
@@ -1,5 +1,13 @@
-#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testingab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing.txt
+#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing
+#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing.txt
 
 
-ab -n 1000 -c 50 http://localhost:8889/testing.txt
+ab -n 1000 -c 10 http://localhost:8889/testing.txt
+# ab -n 1000 -c 50 http://localhost:8889/testing.txt
+# ab -n 1000 -c 100 http://localhost:8889/testing.txt
+# ab -n 1000 -c 150 http://localhost:8889/testing.txt
+# ab -n 1000 -c 200 http://localhost:8889/testing.txt
+# abs -n 100 -c 1 https://localhost:8889/testing.txt
+# abs -n 100 -c 10 https://www.apache.org/
+# abs -l -n 5 https://www.google.com/
 
--- midterm/server_multiprocess.py
+++ midterm/server_multiprocess.py
@@ -1,14 +1,11 @@
-import os
 from socket import *
 import socket
 import threading
 import time
 import sys
 import logging
-import ssl
-
-
 from http import HttpServer
+from multiprocessing import Process
 
 httpserver = HttpServer()
 
@@ -48,36 +45,23 @@ class ProcessTheClient(threading.Thread):
         self.connection.close()
 
 
-class Server(threading.Thread):
-    def __init__(self, hostname='testing.net'):
+class Server(Process):
+    def __init__(self):
         self.the_clients = []
-# ------------------------------
-        self.hostname = hostname
-        cert_location = os.getcwd() + '/certs/'
-        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        self.context.load_cert_chain(certfile=cert_location + 'domain.crt',
-                                     keyfile=cert_location + 'domain.key')
-# ---------------------------------
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        threading.Thread.__init__(self)
+        Process.__init__(self)
 
     def run(self):
         self.my_socket.bind(('0.0.0.0', 8889))
         self.my_socket.listen(1)
         while True:
             self.connection, self.client_address = self.my_socket.accept()
-            try:
-                self.secure_connection = self.context.wrap_socket(
-                    self.connection, server_side=True)
-                logging.warning(
-                    "connection from {}".format(self.client_address))
-                clt = ProcessTheClient(
-                    self.secure_connection, self.client_address)
-                clt.start()
-                self.the_clients.append(clt)
-            except ssl.SSLError as essl:
-                print(str(essl))
+            logging.warning("connection from {}".format(self.client_address))
+
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
 
 
 def main():
