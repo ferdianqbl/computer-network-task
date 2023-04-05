3382490011a5688b996423aab8db3a737ee95810
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-04-05 21:17:10 +0700

    create client_process

--- task_3/client_process.py
+++ task_3/client_process.py
@@ -1,25 +1,16 @@
-import time
-import datetime
+from sys import maxsize
 from multiprocessing import Process
 from kirim_data import kirim_data
 
 
-def runThreadProcess(d=0):
-    procs = []
-    catat_awal = datetime.datetime.now()
-    for k in range(d):
-        waktu = time.time()
-        # bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
-        procs.append(Process(target=kirim_data, args=(k,)))
-        procs[k].start()
-    # setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
-    for k in range(d):
-        procs[k].join()
-    catat_akhir = datetime.datetime.now()
-    selesai = catat_akhir - catat_awal
-    print(
-        f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+def runThreadProcess():
+    processes = []
+    for k in range(maxsize):
+        processes.append(Process(target=kirim_data, args=(k,)))
+        processes[k].start()
+    for k in range(processes):
+        processes[k].join()
 
 
 if __name__ == '__main__':
-    runThreadProcess(20)
+    runThreadProcess()
