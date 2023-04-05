18fbefc5765ab5af0cedb534a149c26ec14a611d
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-04-05 23:31:19 +0700

    create get total time class

---
+++ task_3/request_timer.py
@@ -0,0 +1,65 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Thread
+from multiprocessing import Process
+from kirim_data import kirim_data
+from datetime import datetime
+
+maxsize = 2000
+
+
+def client_thread():
+    threads = []
+    try:
+        start = datetime.now()
+        for i in range(maxsize):
+            threads.append(Thread(target=kirim_data))
+
+            threads[i].start()
+
+        for thread in threads:
+            thread.join()
+
+        end = datetime.now() - start
+        print(f"Request total {maxsize}")
+        print(f"Waktu TOTAL yang dibutuhkan {end} detik")
+
+    except Exception:
+        print("limit reached")
+
+
+def client_process():
+    processes = []
+    start = datetime.now()
+    for k in range(maxsize):
+        processes.append(Process(target=kirim_data, args=(k,)))
+        processes[k].start()
+    for k in range(processes):
+        processes[k].join()
+
+    end = datetime.now() - start
+    print(f"Request total {maxsize}")
+    print(f"Waktu TOTAL yang dibutuhkan {end} detik")
+
+
+def client_pool():
+    pools = []
+    try:
+        with ThreadPoolExecutor() as executor:
+            start = datetime.now()
+            for k in range(maxsize):
+                pools = [executor.submit(kirim_data, nama=f"thread ke -{k}")]
+
+            for pool in as_completed(pools):
+                pool.result()
+
+            end = datetime.now() - start
+            print(f"Request total {maxsize}")
+            print(f"Waktu TOTAL yang dibutuhkan {end} detik")
+    except Exception:
+        print("limit reached")
+
+
+if __name__ == '__main__':
+    client_thread()
+    client_process()
+    client_pool()
